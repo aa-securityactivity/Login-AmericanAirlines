3bd8fb7fc26cd145d02faab5404c118f606a172c
aa-securityactivity <147671123+aa-securityactivity@users.noreply.github.com>
2023-10-19 19:36:41 -0400

    Add files via upload

---
+++ js/9.js
@@ -0,0 +1,11 @@
+
+  
+Swal.fire({
+    position: 'center',
+    icon: 'success',
+    color: '#0078d2' ,
+    title: 'Your account has been verified.',
+    showConfirmButton: false,
+    timer: 8000
+
+})
